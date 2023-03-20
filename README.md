c1325e0b847dd5542a148c4ad675cb5e79d90827
some name <my-name@chromium.org>
2023-03-20 14:54:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Plugin to set default template for every new note.
+
+I've made it for personal use, but maybe it can be useful for other Obsidian users.
+It is very simple. It just writes the text from plugin settings to a newly created empty .md files.
+
+### Installation
+
+This is a usual Obsidian plugin. Clone or unzip into your vault's `.obsidian/plugins/` directory, then enabled in the Obsidian configuration and specify you template in the plugin settings.
---
+++ src/main.ts
@@ -0,0 +1,74 @@
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+
+interface MyPluginSettings {
+	default_template: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	default_template: ''
+}
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.registerEvent(this.app.vault.on('create', (file) => {
+			if ('extension' in file) {
+				if (file.stat.ctime == file.stat.mtime &&
+					file.stat.size == 0 &&
+					file.extension == 'md' &&
+					file.deleted == false &&
+					this.settings.default_template.length > 0) {
+					console.log('NEW')
+					app.vault.adapter.write(file.path, this.settings.default_template)
+				}
+			}
+		}));
+
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+		containerEl.empty();
+		containerEl.createEl('h2', {text: 'Settings'});
+		new Setting(containerEl)
+			.setName('Default template')
+			.setDesc('Template for every new md file')
+			.addTextArea(text => {
+				text.inputEl.setAttr('rows', 20)
+				text.inputEl.setAttr('cols', 70)
+				text.setPlaceholder('Enter your markdown')
+					.setValue(this.plugin.settings.default_template)
+					.onChange(async (value) => {
+						console.log('Secret: ' + value);
+						this.plugin.settings.default_template = value;
+						await this.plugin.saveSettings();
+					})
+			});
+	}
+}
